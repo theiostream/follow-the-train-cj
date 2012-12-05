2b2efb1b6ead8c1067e19ee1813211b767155669
theioztream <daniel.calibeta@gmail.com>
2012-12-05 19:35:57 -0200

    Update

--- baconbot.pl
+++ baconbot.pl
@@ -5,6 +5,16 @@ package BaconBot;
 use Bot::BasicBot;
 @BaconBot::ISA = ('Bot::BasicBot');
 
+sub qsay {
+	my $self = shift;
+	my $channel = shift;
+	my $content = shift;
+	
+	$self->say(
+		channel => $channel,
+		body => $content);
+}
+
 sub said {
 	my $self = shift;
 	my $dict = shift;
@@ -12,9 +22,15 @@ sub said {
 	my $rant = "Channel highlights look exactly the same as unread PM windows. Both go to a phone. Both get attention equally well. If you ask in public to read a private message, you are an attention whore.";
 	
 	if ($dict->{body} =~ m/(check|read|look|see).*PM/i) {
-		$self->say(
-			channel => $dict->{channel},
-			body => "\x02$rant\x02");
+		my $chan = $dict->{channel};
+		my $dude = uc $dict->{who};
+		
+		$self->qsay($chan, "\x02$rant\x02");
+		$self->qsay($chan, "LOOK! $dude! I SENT YOU A MESSAGE!");
+		$self->qsay($chan, "LOOK! I SENT ANOTHER! $dude!");
+		$self->qsay($chan, "$dude! $dude!");
+		
+		$self->qsay($chan, "[BaconBot] Credits to the rant go to DHowett, and creation to theiostream.");
 	}
 }
 
